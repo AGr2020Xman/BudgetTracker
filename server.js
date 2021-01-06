6c30c6ea98526aff9850f219d2b1beb57e949689
Andre Grech <silver.grech@gmail.com>
2021-01-07 01:26:55 +1100

    setting up server connection with mongoDB/mongoose

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = 7000;
 
 const app = express();
 
@@ -15,9 +17,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+const PWD = process.env.DB_PASSWORD;
+const databaseUrl = `mongodb+srv://Andre2020:${encodeURIComponent(
+  PWD
+)}@primarycluster.o092b.mongodb.net/BudgetTracker`;
+mongoose.connect(process.env.MONGODB_URI || databaseUrl, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
